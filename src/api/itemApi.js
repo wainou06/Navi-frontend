4af77f01c04c46b6326eaf180d649a450c2a992b
wainou06 <wainou.6@gmail.com>
2025-08-04 14:41:26 +0900

    메인화면 기초

---
+++ src/api/itemApi.js
@@ -0,0 +1,3 @@
+import naviApi from './axiosApi'
+
+// 전체 상품 리스트 가져오기
---
+++ src/features/authSlice.js
@@ -0,0 +1,12 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+
+// 회원가입
+
+const authSlice = createSlice({
+   name: 'auth',
+   initialState: {},
+   reducers: {},
+   extraReducers: (builder) => {
+      // 회원가입
+   },
+})
