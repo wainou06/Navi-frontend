9b1e8f1b79f0ca3a3af5d0139f9b4bd27d89dc22
wainou06 <wainou.6@gmail.com>
2025-08-05 11:46:16 +0900

    Navbar 완성

--- hmy/navi-frontend/src/App.jsx
+++ hmy/navi-frontend/src/App.jsx
@@ -3,12 +3,24 @@ import Home from './pages/Home'
 import Navbar from './components/shared/Navbar'
 import Footer from './components/shared/Footer'
 
-import { Route, Routes } from 'react-router-dom'
+import { checkAuthStatusThunk } from './features/authSlice'
+
+import { Route, Routes, useLocation } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { useEffect } from 'react'
 
 function App() {
+   const dispatch = useDispatch()
+   const location = useLocation()
+   const { isAuthenticated, user } = useSelector((state) => state.auth)
+
+   useEffect(() => {
+      dispatch(checkAuthStatusThunk())
+   }, [dispatch])
+
    return (
       <>
-         <Navbar />
+         <Navbar isAuthenticated={isAuthenticated} user={user} />
          <Routes>
             <Route path="/" element={<Home />} />
          </Routes>
